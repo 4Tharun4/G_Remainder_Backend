07040437873f014b036c7e924596f5fde3668c48
Tharun G V <tharungv4@gmail.com>
2024-12-18 22:19:36 +0530

    first commit

--- dist/routers/authroute.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const authentication_1 = require("../controllers/authentication");
-exports.default = (router) => {
-    //@ts-ignore
-    router.post('/auth/login', authentication_1.register);
-};
-//# sourceMappingURL=authroute.js.map
